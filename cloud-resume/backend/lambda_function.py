56120d77fdfd8634cdd204e7f32516a0198a5b77
Agastya <189466251+foureyes001@users.noreply.github.com>
2026-06-27 23:53:24 +0530

    add visitor-counter lambda + least-priv IAM policy

---
+++ cloud-resume/backend/lambda_function.py
@@ -0,0 +1,22 @@
+import boto3
+import json
+
+dynamodb=boto3.resource('dynamodb')
+table= dynamodb.Table('cloud-resume-visitor-count')
+
+def lambda_handler(event,context):
+    response = table.update_item(
+    Key={
+            'id':'visitors'},
+    UpdateExpression='ADD #c :inc',
+    ExpressionAttributeNames={
+        '#c': 'count'},
+    ExpressionAttributeValues={
+        ':inc':1},
+    ReturnValues='UPDATED_NEW' )
+    new_count=int(response['Attributes']['count'])    
+    return {
+    'statusCode': 200,
+    'headers': { 'Access-Control-Allow-Origin': '*' },   
+    'body': json.dumps({ 'count': new_count })            
+    }
